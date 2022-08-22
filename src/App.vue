15ef6fa02979e2ab24a7538613b522449911faa3
Yisraelyoav <yisralyoav@gmail.com>
2022-08-22 19:39:13 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default{
+  data() {
+return {
+  todos:[{
+  title: 'Build an awesome 2Do app',
+  description: 'dazzeling app that will get you a job',
+  deadline: new Date,
+  repetition: '1yr',
+  priority:'High',  
+  },
+  {
+  title: 'Get a job',
+  description: 'finally get an amazing job',
+  deadline: new Date,
+  repetition: '1yr',
+  priority:'High',  
+  }]
+}
+  },
+  computed(){
+
+  },
+  methods(){
+
+  }
+}
+</script>
+
+<style>
+
+</style>
